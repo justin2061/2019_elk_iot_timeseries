311d958b05fb45bab74bfbbe92249dc5851f2b41
Justin <madoka2000@gmail.com>
2020-06-29 15:30:26 +0800

    ip 2 localhost

--- re_import_covid19_data.sh
+++ re_import_covid19_data.sh
@@ -2,7 +2,7 @@
 docker-compose stop logstash
 rm -f logstash/data/COVID-19_sincedb
 rm -f logstash/data/COVID-19_new_sincedb
-curl -XDELETE 'http://139.162.75.164:9200/covid-19'
-curl -XDELETE 'http://139.162.75.164:9200/covid-19_1'
+curl -XDELETE 'http://localhost:9200/covid-19'
+curl -XDELETE 'http://localhost:9200/covid-19_1'
 docker-compose start logstash
 
